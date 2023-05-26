2398a5984d22c5538e2673b22caf3ad79bdd98b1
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-05-26 16:50:11 +0530

    new components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-   <div>
-
-   </div>
+    <div className="App">
+      <h1>My Awesome Blog</h1>
+      <div id="page-body">
+        Welcome to my blog!
+      </div>
+    </div>
   );
 }
 
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ArticlesListPage() {
+  return (
+    <div>ArticlesListPage</div>
+  )
+}
