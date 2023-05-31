83c6dacec7e1f3f3c61fc4ab32b1c3e533f1cc46
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-01 01:11:16 +0530

    updated

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,16 +1,27 @@
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
+import {useState, useEffect} from 'react';
+
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleinfo] = useState({upvotes:0,comments:[]});
+
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
+    
+    useEffect(() =>{
+        setArticleinfo({upvotes:3, comments:[]});
+    },[]);
+    
     if(!article){
         <NotFoundPage/>
     }
+
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
